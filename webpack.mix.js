14e9a7ea5b61690825b277764811d41d38abaa12
Milan <milan.jansen@bitfactory.nl>
2024-04-03 11:30:18 +0200

    Nova 4 & Vue 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,16 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
-mix.setPublicPath('dist').js('resources/js/translatable-field.js', 'js').vue();
-mix.setPublicPath('dist').js('resources/js/locale-select-field.js', 'js').vue();
+mix
+  .setPublicPath('dist')
+  .js('resources/js/translatable-field.js', 'js')
+  .js('resources/js/locale-select-field.js', 'js')
+  .vue({ version: 3 })
+  .webpackConfig({
+    output: {
+      externals: {
+        vue: 'Vue',
+        'laravel-nova': 'LaravelNova',
+      },
+      uniqueName: 'bitfactory/nova-translatable',
+    },
+  })
